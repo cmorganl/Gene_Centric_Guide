aabf654c1babaa4d98478cd808007ce4dcaf4627
cmorganl <c.morganlang@gmail.com>
2021-03-30 13:33:11 +0200

    Update CAMI commands, add bibliography and supplementary tables

--- CAMI_experiments/commands.sh
+++ CAMI_experiments/commands.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-treesapp assign \
--i /mnt/sdb/Hallam_projects/Hallam_Databases/formatted/CAMI_I_HIGH/gold_standard_high_single.fasta.gz \
--o CAMI_experiments/gold_standard_high_single/ \
---refpkg_dir CAMI_experiments/refpkgs/ \
---hmm_coverage 10 \
---num_procs 16 \
---trim_align --overwrite
+file_array=("/mnt/sdb/Hallam_projects/Hallam_Databases/formatted/CAMI_I_HIGH/gold_standard_high_single.fasta.gz"
+"CAMI_experiments/RH_S001_merged.fq"
+"CAMI_experiments/RH_S001_forward.fq")
 
-treesapp assign \
--i CAMI_experiments/RH_S001_merged.fq \
--o CAMI_experiments/RH_S001_merged/ \
---refpkg_dir CAMI_experiments/refpkgs/ \
---hmm_coverage 10 \
---num_procs 16 \
---trim_align --overwrite
+outputs_array=()
 
-treesapp assign \
--i CAMI_experiments/RH_S001.1.fq \
--o CAMI_experiments/RH_S001_forward/ \
+for f in "${file_array[@]}"
+do
+  assign_output=$( basename $f | sed 's/.fq.*//g' | sed 's/.fasta.*//g' )
+  treesapp assign \
+-i $f \
+-o CAMI_experiments/${assign_output} \
 --refpkg_dir CAMI_experiments/refpkgs/ \
 --hmm_coverage 10 \
 --num_procs 16 \
 --trim_align --overwrite
+  outputs_array+=("CAMI_experiments/${assign_output}")
+done
 
 for f in CAMI_experiments/refpkgs/*pkl
 do
 	rp=$( basename $f | sed 's/_build.pkl//g' )
-	treesapp phylotu --refpkg_path $f --assign_output CAMI_experiments/gold_standard_high_single/ -o CAMI_experiments/gold_standard_high_single/phylotu_out_${rp}/
-	treesapp phylotu --refpkg_path $f --assign_output CAMI_experiments/RH_S001_merged/ -o CAMI_experiments/RH_S001_merged/phylotu_out_${rp}/
-	treesapp phylotu --refpkg_path $f --assign_output CAMI_experiments/RH_S001_forward/ -o CAMI_experiments/RH_S001_forward/phylotu_out_${rp}/
+	for d in "${outputs_array[@]}"
+	do
+	  treesapp phylotu --refpkg_path $f --assign_output $d -o ${d}/phylotu_out_${rp}/
+	done
 done
-
