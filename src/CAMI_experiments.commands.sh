32a3297a2b2286fbb990e5000fd5215890921baa
cmorganl <c.morganlang@gmail.com>
2021-06-11 10:15:03 +0200

    Added CLI options and doesn't overwrite by default, just reruns clustering

--- src/CAMI_experiments.commands.sh
+++ src/CAMI_experiments.commands.sh
@@ -1,24 +1,71 @@
 #!/bin/bash
 
-file_array=("/mnt/sdb/Hallam_projects/Hallam_Databases/formatted/CAMI_I_HIGH/gold_standard_high_single.fasta.gz"
-"../CAMI_experiments/RH_S001_merged.fq"
-"../CAMI_experiments/RH_S001_forward.fq")
+default_dir="../CAMI_experiments"
 
-analysis_dir="../CAMI_experiments/"
+usage="
+USAGE:\n
+                $0 [path/to/CAMI_experiments/=$default_dir] [-t num_threads=16] [-o overwrite='n']
+\n"
+
+if [ $1 == "-h" ]; then
+	echo -e $usage
+	exit
+fi
+
+# Set arguments
+if [ ! -z $1 ]; then
+  analysis_dir=$1
+else
+  analysis_dir=$default_dir
+fi
+
+while getopts ":t:o:" opt; do
+  case $opt in
+  t) n_cpu="$OPTARG"
+    ;;
+  o) overwrite="$OPTARG"
+    ;;
+  \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+if [ -z $n_cpu ]; then
+  n_cpu=16
+fi
+if [ -z $overwrite ]; then
+  overwrite="n"
+fi
+
 analysis_refpkgs=${analysis_dir}/refpkgs/
-n_cpu=16
+file_array=("/mnt/sdb/Hallam_projects/Hallam_Databases/formatted/CAMI_I_HIGH/gold_standard_high_single.fasta.gz"
+"${analysis_dir}/RH_S001_merged.fq"
+"${analysis_dir}/RH_S001_forward.fq")
 outputs_array=()
+
+if [ ! -d $analysis_refpkgs ]; then
+  echo "ERROR: Unable to find directory with reference packages '${analysis_refpkgs}'"
+  exit
+fi
 
 for f in "${file_array[@]}"
 do
+  if [ ! -f $f ]; then
+    echo "ERROR: Unable to find input file '${f}'"
+    exit
+  fi
+
   assign_output=$analysis_dir/$( basename $f | sed 's/.fq.*//g' | sed 's/.fasta.*//g' )
-  treesapp assign \
-  -i $f \
-  -o ${assign_output} \
-  --refpkg_dir ${analysis_refpkgs} \
-  --num_procs $n_cpu \
-  --trim_align \
-  --overwrite --delete
+  if [ -d ${assign_output} ] && [ $overwrite == 'n' ]; then
+    echo "Skipping classification of '${f}'"
+  else
+    treesapp assign \
+    -i $f \
+    -o ${assign_output} \
+    --refpkg_dir ${analysis_refpkgs} \
+    --num_procs $n_cpu \
+    --trim_align \
+    --overwrite --delete
+  fi
   outputs_array+=(${assign_output})
 done
 
